59c1e2c81321f564d09af380f4eaf4027175f4ce
Irek <ibigaj+github@gmail.com>
2021-11-10 06:25:16 +0100

    Dodanie kolejnych metod pracy ze slownikiem w Pythonie

--- urodziny.py
+++ urodziny.py
@@ -1,4 +1,7 @@
 # Programik zapaiętujący daty urodzin bazujący na słowniku
+import introOutro
+
+introOutro.intro()
 
 birthdays = {'Alicja': '1 kwietnia', 'Bob': '12 grudnia', 'Karol': '4 marca'}
 
@@ -15,3 +18,31 @@ while True:
         bday = input()
         birthdays[name] = bday
         print('Data urodzin została zaktualizowana')
+
+# metoda values() dla słownika zwraca wszystkie wartości ze słownika
+# może być wykorzystana w pętli for, ale nie jest listą - tak jak i poniższe dwie
+# metoda keys() robi analogicznie dla kluczy
+# metoda items() wyświetla pary klucz-wartość
+# można wyniki tych 3 metod zamienić na liste podstawiając do funkcji list()
+
+print('No to podsumowanie:')
+print('Wyświetlmy wszytkie daty urodzin:')
+for k in birthdays.values():
+    print(k)
+
+print('Wyświetlmy wszystkie imiona:')
+for i in birthdays.keys():
+    print(i)
+
+print('Wyświetlmy cały kalendarz:')
+for j in birthdays.items():
+    print(j)
+
+print(list(birthdays.values()))
+
+# przypisanie do dwóch zmiennych klucza i wartości ze słownika
+
+for m, n in birthdays.items():
+    print('Klucz: ' + m + ', wartość: ' + n + '.')
+
+introOutro.outro()
