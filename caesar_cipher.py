bfc44013e0d6fff22e7d7d33a06bb3625323d37a
Irek <ibigaj+github@gmail.com>
2021-11-29 15:19:49 +0100

    #100days of code - day 8 - Caesar Cipher

---
+++ caesar_cipher.py
@@ -0,0 +1,53 @@
+# #100days of code - day 8 - Caesar Cipher
+
+logo = """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""""""  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP""""""" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+def caesar(direct, text_to_change, shift_amount):
+    new_text = ""
+    if direct.lower() == "decode":
+        shift_amount *= -1
+    for character in text_to_change:
+        if character in alphabet:
+            alphabet_index = alphabet.index(character)
+            new_index = alphabet_index + shift_amount
+            new_text += alphabet[new_index]
+        else:
+            new_text += character
+    print(f"The {direct}d text is {new_text}")
+
+
+print(logo)
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+    caesar(direction, text, shift)
+
+    result = input("Type 'yes' if you want to go again. Otherwise type 'no':\n").lower()
+    if result == "no":
+        should_continue = False
+        print("Goodbye")
