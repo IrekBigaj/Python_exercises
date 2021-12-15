c215dd155e9df91b75f190192be8330ca72a7de7
Irek <ibigaj+github@gmail.com>
2021-12-15 09:43:01 +0100

    # #100days of code - day 24 - exercises

---
+++ Day24/main.py
@@ -0,0 +1,22 @@
+# #100days of code - day 24 - Exercises
+
+# file = open("my_text_file.txt")
+# content = file.read()
+# print(content)
+# file.close()
+
+# with open("my_text_file.txt") as file:
+#     print(file.read())
+#
+# with open("my_text_file.txt", "a") as file:
+#     file.write("\nNowa linia")
+
+
+with open("C:\\Users\\Irek\\Desktop\\AAA.txt") as file:
+     print(file.read())
+
+with open("/Users/Irek/Desktop/AAA.txt") as file:
+    print(file.read())
+
+with open("../../../Desktop/AAA.txt") as file:
+    print(file.read())
